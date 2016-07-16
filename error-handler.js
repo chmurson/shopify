da5544a14bddafb685c3f3fbef27c0723f194f2d
Wojciech Piątkowski <chmurson@gmail.com>
2016-07-16 11:23:14 +0200

    publish: fixed selecting paczkomat
    generated from commit a185d88b5015fb2deb865c161247ae335e96387e

--- error-handler.js
+++
@@ -1,7 +0,0 @@
-window.onerror = function(message, source, lineno, colno, error) {
-  console.log(message);
-  console.log(source);
-  console.log(lineno);
-  console.log(colno);
-  console.log(error);
-};
