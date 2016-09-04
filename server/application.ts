749796dd2b4572ff4a32aafb89db08075f72a375
Wojciech Piątkowski <chmurson@gmail.com>
2016-09-04 22:34:33 +0200

    webhook authentication

--- server/application.ts
+++ server/application.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 
+const rawBodyParser = require('body-parser-rawbody');
+
 const application = express();
-application.use(bodyParser.json());
+application.use(rawBodyParser.json());
 application.use(cors());
 
 export default application;
--- server/webhookAuth.ts
+++ server/webhookAuth.ts
@@ -2,33 +2,15 @@ import {createHmac} from 'crypto';
 import {SHOPIFY_SHARED_SECRET} from './constants';
 import {get as getEnv} from './env';
 
-var getRawBody = require('raw-body');
-var typer = require('media-typer');
-
 export function webhookAuth(req, res, next) {
+  const createdHmac = createHmac("SHA256", getEnv(SHOPIFY_SHARED_SECRET)).update(new Buffer(req.rawBody, 'utf8')).digest('base64');
+  const hmacHeader = req.get('HTTP_X_SHOPIFY_HMAC_SHA256');
 
+  if (createdHmac !== hmacHeader) {
+    console.warn('HTTP_X_SHOPIFY_HMAC_SHA256 header does not match calculated HMAC');
+    return res.sendStatus(401);
+  }
 
-  getRawBody(req, {
-    length: req.headers['content-length'],
-    limit: '1mb',
-    encoding: typer.parse(req.headers['content-type']).parameters.charset
-  }, function (err, string) {
-    console.log('fetching data has ended');
-    if (err) {
-      console.error("Could not fetch raw body");
-      console.error(err);
-      return res.sendStatus(401);
-    }
-
-    const createdHmac = createHmac("SHA256", getEnv(SHOPIFY_SHARED_SECRET)).update(new Buffer(string, 'utf8')).digest('base64');
-    const hmacHeader = req.get('HTTP_X_SHOPIFY_HMAC_SHA256');
-
-    if (createdHmac !== hmacHeader) {
-      console.warn('HTTP_X_SHOPIFY_HMAC_SHA256 header does not match calculated HMAC');
-      return res.sendStatus(401);
-    }
-
-    next()
-  });
+  next()
 }
 
