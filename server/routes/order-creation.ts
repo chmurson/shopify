a378438f67019083490d4190defa30edd811af65
Wojciech Piątkowski <chmurson@gmail.com>
2016-09-04 22:36:01 +0200

    webhook authentication

--- server/routes/order-creation.ts
+++ server/routes/order-creation.ts
@@ -10,7 +10,7 @@ import {createFailureJson, createSuccessJson} from './../jsonResponses';
 /**
  * This is hook for shopify. Requested once checkout is finished (user goes to thank you page), which is creation of order.
  */
-application.post('/order-creation', webhookAuth, requestHandler);
+application.post('/order-creation', requestHandler);
 
 function requestHandler(req:express.Request, res:express.Response, next:express.NextFunction) {
   //@todo here should be API verification
