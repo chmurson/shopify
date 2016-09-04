43b2f7653b47ec4bf71ac1a22118fc2b7d19f226
Wojciech Piątkowski <chmurson@gmail.com>
2016-09-04 22:22:30 +0200

    webhook authentication

--- server/webhookAuth.ts
+++ server/webhookAuth.ts
@@ -2,31 +2,31 @@ import {createHmac} from 'crypto';
 import {SHOPIFY_SHARED_SECRET} from './constants';
 import {get as getEnv} from './env';
 
+var getRawBody = require('raw-body');
+var typer = require('media-typer')
+
 export function webhookAuth(req, res, next) {
-  let rawBody = '';
-  req.setEncoding('utf8');
   console.log('fetching data has started!');
 
-  req.on('data', function (chunk) {
-    rawBody += chunk;
-  });
-
-  req.on('end', function () {
-    console.log('fetching data has ended!');
-
-    const createdHmac = createHmac("SHA256", getEnv(SHOPIFY_SHARED_SECRET)).update(new Buffer(req.rawBody, 'utf8')).digest('base64');
-    const hmacHeader = req.get('HTTP_X_SHOPIFY_HMAC_SHA256');
-
-    if (createdHmac !== hmacHeader) {
-      console.log('HTTP_X_SHOPIFY_HMAC_SHA256 header does not match calculated HMAC');
+  getRawBody(req, {
+    length: req.headers['content-length'],
+    limit: '1mb',
+    encoding: typer.parse(req.headers['content-type']).parameters.charset
+  }, function (err, string) {
+    if (err) {
+      console.error(err);
       return res.sendStatus(401);
     }
 
-    next();
+    const createdHmac = createHmac("SHA256", getEnv(SHOPIFY_SHARED_SECRET)).update(new Buffer(string, 'utf8')).digest('base64');
+    const hmacHeader = req.get('HTTP_X_SHOPIFY_HMAC_SHA256');
+
+    if (createdHmac !== hmacHeader) {
+      console.warn('HTTP_X_SHOPIFY_HMAC_SHA256 header does not match calculated HMAC');
+      return res.sendStatus(401);
+    }
+
+    next()
   });
-}
-
-function auth() {
-
 }
 
